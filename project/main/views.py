ebfe06087cdd4dc1cc36ba303fadb26e1b21212b
Ahmet CANGIR <ahmetcangir07@gmail.com>
2019-04-16 20:04:29 +0200

    Problem fixed

--- project/main/views.py
+++ project/main/views.py
@@ -1,51 +1,8 @@
 # project/main/views.py
-
-# Api KEY: 7FOHNX2L98IZ
-
-
 from flask import render_template, Blueprint, redirect, url_for, jsonify
 from .forms import SearchForm
 import requests
 import json
-import urllib.request,urllib.parse,urllib.error
-import pprint
-
-# set the apikey and limit
-apikey = "7FOHNX2L98IZ"  # test value
-lmt = 8
-
-# load the user's anonymous ID from cookies or some other disk storage
-# anon_id = <from db/cookies>
-
-# ELSE - first time user, grab and store their the anonymous ID
-r = requests.get("https://api.tenor.com/v1/anonid?key=%s" % apikey)
-
-if r.status_code == 200:
-    anon_id = json.loads(r.content)["anon_id"]
-    # store in db/cookies for re-use later
-else:
-    anon_id = ""
-
-# our test search
-search_term = "CodeDoor"
-
-# get the top 8 GIFs for the search term
-r = requests.get(
-    "https://api.tenor.com/v1/search?q=%s&key=%s&limit=%s&anon_id=%s" %   
-     (search_term, apikey, lmt, anon_id))
-
-if r.status_code == 200:
-    # load the GIFs using the urls for the smaller GIF sizes
-    pp = pprint.PrettyPrinter(indent=4)
-    gifs_result = json.loads(r.content)
-    # pp.pprint(top_8gifs['results']) #pretty prints the json file.
-
-    # for i in range(len(top_8gifs['results'])):
-        # url = top_8gifs['results'][i]['media'][0]['gif']['url'] #This is the url from json.
-        # print (url)
-        # urllib.request.urlretrieve(url, str(i)+'.gif') #Downloads the gif file.
-else:
-    gifs_result = None
 
 main_blueprint = Blueprint('main', __name__,)
 
@@ -61,11 +18,36 @@ def search():
 
 @main_blueprint.route("/results")
 def search_results(search_query):
-    return render_template('main/results.html', results=gifs_result,
+    return render_template('main/results.html', results=query_API(search_query),
                            query=search_query)
 
 
 def query_API(searchterm):
-    tenor_apikey = None
-    return tenor_apikey
+    tenor_apikey = "7FOHNX2L98IZ"
 
+    # specify the anonymous_id tied to the given user
+    # load the user's anonymous ID from cookies or some other disk storage
+    # anon_id = <from db/cookies>
+    # ELSE - first time user, grab and store their the anonymous ID
+    r = requests.get("https://api.tenor.com/v1/anonid?key=%s" % tenor_apikey)
+
+    if r.status_code == 200:
+        anon_id = json.loads(r.content)["anon_id"]
+        # store in db/cookies for re-use later
+    else:
+        anon_id = ""
+
+    # get the GIFs for the search term
+    r = requests.get(
+        "https://api.tenor.com/v1/search?q=%s&key=%s&anon_id=%s" %
+        (searchterm, tenor_apikey, anon_id))
+
+    if r.status_code == 200:
+        # load the GIFs using the urls for the smaller GIF sizes
+        gifs_result = json.loads(r.content)
+        # pp = pprint.PrettyPrinter(indent=4)
+        # pp.pprint(gifs_result) #pretty prints the json file.
+    else:
+        gifs_result = None
+
+    return gifs_result
